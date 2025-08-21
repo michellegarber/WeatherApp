4d98acca3f0fea5c1029cd55b406d6446392a9a6
Matias Dubin <magondubi@gmail.com>
2025-08-21 11:48:04 -0300

    inicios

--- WeatherApp/src/contexts/ThemeContext.jsx
+++ WeatherApp/src/contexts/ThemeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect } from "react";
+/**/import { createContext, useState, useContext, useEffect } from "react";
 import "./contexts/ThemeContext.css";
 
 const ThemeContext = createContext();
